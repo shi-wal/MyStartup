df3b422ed2fc93c5600401b269ea0c9c8b964025
shi-wal <shivangiagrawal667@gmail.com>
2026-04-21 18:22:11 +0530

    feat: implemented backend structure including product routes, model, middleware, and data seeder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors=require("cors");
 const dotenv=require("dotenv");
 const connectDB=require("./config/db");
 const userRoutes=require("./routes/userRoutes");
+const productRoutes=require("./routes/productRoutes");
 
 const app=express();
 app.use(express.json());
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 
 //API routes
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
